c22706f099132200a8fe25e0d4d87b0795568add
prudhvi2467 <100276368+prudhvi2467@users.noreply.github.com>
2024-05-28 08:02:58 -0700

    Add files via upload

---
+++ Nxt-watch-app-main/Nxt-watch-app-main/src/components/VideoItemDetails/styledComponents.js
@@ -0,0 +1,8 @@
+import Styled from 'styled-components'
+
+export const LikeButton = Styled.button`
+color: ${props => props.color};
+`
+export const VideoItemContainer = Styled.div`
+background-color:${props => props.bgColor};
+color : ${props => props.color}`
